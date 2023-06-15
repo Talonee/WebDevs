70576a6c3a09107f653144ef71acc156b82d88fa
Talon Pham <34594908+Talonee@users.noreply.github.com>
2023-06-15 09:25:34 -0700

    First attempt in Svelte conversion. Omfg I forgot everything.

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -15,7 +15,7 @@ search.addEventListener('click', () => {
 		response => response.json()).then(
 		json => {
 		if(json.cod === '404') {
-			container.style.height = '400px';
+			container.style.height = '450px';
 			weatherBox.style.display = 'none';
 			weatherDetails.style.display = 'none';
 			error404.style.display = 'block';
